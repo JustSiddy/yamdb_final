69c124b5e5e0a3b53e874d6501fd89c926e24240
Сидни <just_siddy@mail.ru>
2023-03-09 21:22:03 +0300

    rew2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -110,9 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
-STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static')),
-                    ('redoc', '/app/static/')]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
