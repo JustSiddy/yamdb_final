44f0ef7f0fd1c3fa477f059bfb3924ed5a32fc3b
Сидни <just_siddy@mail.ru>
2023-03-09 20:55:25 +0300

    rew2

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,7 +2,6 @@ from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-
 router_v1 = DefaultRouter()
 router_v1.register('posts', PostViewSet, basename='posts')
 router_v1.register('groups', GroupViewSet, basename='groups')
