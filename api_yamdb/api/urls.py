5c627858ace696d78c074d54330f90d838d757e0
Сидни <just_siddy@mail.ru>
2023-03-09 20:38:18 +0300

    rew2

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,4 +1,4 @@
-from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
