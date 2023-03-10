6755b5f9c338109d09130c34f3f88a80365b9ff8
Сидни <just_siddy@mail.ru>
2023-03-10 19:41:57 +0300

    rew2

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -54,9 +54,9 @@ class Title(models.Model):
     )
 
     class Meta:
-        constraints = models.CheckConstraint(
+        constraints = [models.CheckConstraint(
             check=Q(year__lte=dt.datetime.today().year),
-            name='year__lte=now_year')
+            name='year__lte=now_year')]
         verbose_name_plural = 'Произведения'
 
     def __str__(self):
